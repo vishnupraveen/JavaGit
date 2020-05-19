7180e21354db0fec1d6c3433342e2a42c0a8b91b
Vishnu_Praveen <vishnupraveenblr@gmail.com>
2020-05-19 21:18:26 +0530

    Initial Commit

---
+++ src/collections/HashMapImpl.java
@@ -0,0 +1,19 @@
+package collections;
+
+import java.util.*;
+
+public class HashMapImpl {
+
+    public static void main(String[] args) {
+        Map<String ,String> map=new HashMap<>();
+        map.put("Karnataka","Bengaluru");
+        map.put("Maharashtra","Mumbai");
+        map.put("Tamil Nadu","Chennai");
+
+        System.out.println(map);
+        Set<String> keys=map.keySet();
+        Iterator<String> itr=keys.iterator();
+
+        System.out.println(map);
+    }
+}
